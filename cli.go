0ab9c6d3b3284d0d4a966fe1347b9cfa25f17d0e
Luciano Nooijen <luciano@bytecode.nl>
2019-10-16 15:35:45 +0200

    Added templates and generation
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- cli.go
+++ cli.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"text/template"
 )
 
 func main() {
@@ -12,7 +15,8 @@ func main() {
 
 func flagSetupAndHandler () {
 	printHelp := flag.Bool("help", false, "Command usage")
-	fileType := flag.String("type", "cm", "file type, 'cm' for component, 'cn' for container, 'p' for page")
+	sourceDirectory := flag.String("dir", "src", "Directory in which the component should be created (a subdirectory will be created)")
+	componentName := flag.String("name", "TestComponent", "The name of the component to be created")
 	useRedux := flag.Bool("redux", false, "Connect new file to Redux")
 
 	flag.Parse()
@@ -22,7 +26,53 @@ func flagSetupAndHandler () {
 		os.Exit(0)
 	}
 
-	fmt.Println(*printHelp)
-	fmt.Println(*fileType)
-	fmt.Println(*useRedux)
+	saveTemplateFiles(*sourceDirectory, *componentName, *useRedux)
 }
+
+type fileTemplate struct {
+	ComponentName string
+}
+
+func generateTemplatedString (name string, templateBase string, fileTemplate fileTemplate) string {
+	t := template.New(name)
+	t, err := t.Parse(templateBase)
+	if err != nil {
+		log.Fatal("Parse: ", err)
+	}
+	tBuffer := new(bytes.Buffer)
+	err = t.Execute(tBuffer, fileTemplate)
+	if err != nil {
+		log.Fatal("Execute: ", err)
+	}
+	return tBuffer.String()
+}
+
+func saveTemplateFiles (sourceDirectory string, componentName string, useRedux bool) {
+	templateInfo := fileTemplate{ComponentName: componentName}
+
+	if useRedux {
+		mainFile := generateTemplatedString("Main", templateMainFileWithRedux, templateInfo)
+		testFile := generateTemplatedString("Test", templateTestFileWithRedux, templateInfo)
+		fmt.Println(mainFile)
+		fmt.Println(testFile)
+	} else {
+		mainFile := generateTemplatedString("Main", templateMainFileNoRedux, templateInfo)
+		testFile := generateTemplatedString("Test", templateTestFileNoRedux, templateInfo)
+		fmt.Println(mainFile)
+		fmt.Println(testFile)
+	}
+
+	componentsFile := generateTemplatedString("Components", templateComponentsFile, templateInfo)
+	fmt.Println(componentsFile)
+
+	typesFile := generateTemplatedString("Components", templateTypesFile, templateInfo)
+	fmt.Println(typesFile)
+
+	fmt.Println(sourceDirectory)
+	fmt.Println(componentName)
+	fmt.Println(useRedux)
+}
+
+//func saveSingleTemplate (templatedString string, fileDest string) {
+//
+//}
---
+++ templates.go
@@ -0,0 +1,78 @@
+package main
+
+const templateMainFileNoRedux = `import React from 'React';
+import { {{.ComponentName}}Container } from './{{.ComponentName}}.components';
+import {{.ComponentName}}Props from './import {{.ComponentName}}.types';
+
+const {{.ComponentName}}: React.FC<{{.ComponentName}}Props> = ({ text }) => {
+	return (
+		<{{.ComponentName}}Container>
+			{name}
+		</{{.ComponentName}}Container>
+	);
+};
+
+export default {{.ComponentName}};
+`
+
+const templateMainFileWithRedux = `import React from 'React';
+import { connect } from 'react-redux';
+import { {{.ComponentName}}Container } from './{{.ComponentName}}.components';
+import {{.ComponentName}}Props from './import {{.ComponentName}}.types';
+
+export const {{.ComponentName}}: React.FC<{{.ComponentName}}Props> = ({ text }) => {
+	return (
+		<{{.ComponentName}}Container>
+			{name}
+		</{{.ComponentName}}Container>
+	);
+};
+
+const mapStateToProps = (state: ReduxState) => ({ // TODO: Add the ReduxState type
+	name: state.name,
+});
+const mapDispatchToProps = {};
+const Connected{{.ComponentName}} = connect(
+	mapStateToProps,
+	mapDispatchToProps,
+)({{.ComponentName}});
+
+export default Connected{{.ComponentName}};
+`
+
+const templateTestFileNoRedux = `import React from 'react';
+import { mount } from 'enzyme';
+import {{.ComponentName}} from './{{.ComponentName}}';
+	
+describe('{{.ComponentName}}', () => {
+	it('should render', () => {
+		const wrapper = mount(<{{.ComponentName}} name="test"/>);
+		expect(wrapper.type()).toEqual({{.ComponentName}});
+	});
+});
+`
+
+const templateTestFileWithRedux = `import React from 'react';
+import { mount } from 'enzyme';
+import { {{.ComponentName}} } from './{{.ComponentName}}';
+	
+describe('{{.ComponentName}} without Redux', () => {
+	it('should render', () => {
+		const wrapper = mount(<{{.ComponentName}} name="test"/>);
+		expect(wrapper.type()).toEqual({{.ComponentName}});
+	});
+});
+`
+
+const templateTypesFile = `interface {{.ComponentName}}Props {
+	name: string
+}
+
+export default {{.ComponentName}}Props;
+`
+
+const templateComponentsFile = `import styled from 'styled-components';
+
+export const {{.ComponentName}}Container = styled.div` + "`" + `
+	margin: 10px;
+` + "`;\n"
