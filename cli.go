86f4d740e68a187b4dac8179406f469710818e23
Luciano Nooijen <luciano@bytecode.nl>
2019-10-16 13:57:35 +0200

    Added flags
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- cli.go
+++ cli.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("test")
+	flagSetupAndHandler()
+}
+
+func flagSetupAndHandler () {
+	printHelp := flag.Bool("help", false, "Command usage")
+	fileType := flag.String("type", "cm", "file type, 'cm' for component, 'cn' for container, 'p' for page")
+	useRedux := flag.Bool("redux", false, "Connect new file to Redux")
+
+	flag.Parse()
+
+	if *printHelp {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	fmt.Println(*printHelp)
+	fmt.Println(*fileType)
+	fmt.Println(*useRedux)
 }
