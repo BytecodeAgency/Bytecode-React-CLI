fe21ad7734e92063f7a5910315d4610eb6071419
Luciano Nooijen <luciano@bytecode.nl>
2020-04-02 16:35:46 +0200

    WIP on ReduxDomain stuff
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- templates/reduxDomain.js
+++ templates/reduxDomain.js
@@ -15,5 +15,25 @@ const action = (name) => `action`;
 const actionTest = (name) => `actionTest`;
 const reducer = (name) => `reducer`;
 const reducerTest = (name) => `reducerTest`;
-const types = (name) => `types`;
-const constants = (name) => `constants`;
+const types = (name) => `import ${capitalizeFirstLetter(name)}Actions from './${name}.constants.ts';
+
+export default interface ${capitalizeFirstLetter(name)}State {
+    errors: string[] | null;
+}
+
+interface DispatchArguments<T, R> { // TODO: Improve interface, use the built-in Redux Dispatch
+    type: T;
+    payload?: R;
+}
+type Dispatch<T, R> = (argument: DispatchArguments<T, R>) => void;
+
+export type ${capitalizeFirstLetter(name)}Dispatch = ${capitalizeFirstLetter(name)}AddError // | More types
+
+export type ${capitalizeFirstLetter(name)}AddError = Dispatch<${capitalizeFirstLetter(name)}Actions.AddError, string>
+`;
+
+const constants = (name) => `export default enum ${capitalizeFirstLetter(name)}Actions {
+    AddError = "AddError",
+}`;
+
+const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
